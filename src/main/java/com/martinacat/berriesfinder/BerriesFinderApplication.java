a78b5744278df6f65cc864b65c791796d2e4a2be
Martina Catizone <martina.catizone@gmail.com>
2020-04-18 08:32:31 +0100

    update .gitignore and project structure

---
+++ src/main/java/com/martinacat/berriesfinder/BerriesFinderApplication.java
@@ -0,0 +1,13 @@
+package com.martinacat.berriesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerriesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerriesFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martinacat/berriesfinder/BerriesFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martinacat.berriesfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerriesFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
